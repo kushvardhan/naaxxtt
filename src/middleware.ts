bd4520f153dbcdf99b47da9651f3e0197f9e8b0e
Kush Vardhan <kushvardhan39797@gmail.com>
2025-05-17 20:20:57 +0530

    Setting up the middleware and all the secure key and URLs

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const publicRoutes = [
-  '/',
-];
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)','/sign-up(.*)','/'])
 
-export default clerkMiddleware({
-  publicRoutes,
-});
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect()
+  }
+})
 
 export const config = {
   matcher: [

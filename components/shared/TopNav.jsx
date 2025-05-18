cbef9c787ee955726f28ccf97f3f901b01b75afe
Kush Vardhan <kushvardhan39797@gmail.com>
2025-05-18 21:21:52 +0530

    Update TopNav component layout and integrate UserButton; add Space_Grotesk font to layout

--- components/shared/TopNav.jsx
+++ components/shared/TopNav.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React,{ useContext } from 'react'
 import Link from 'next/link'
+import { SignedIn,UserButton } from '@clerk/nextjs';
 import { ThemeContext } from '../../context/ThemeContext';
 
 const TopNav = () => {
@@ -9,18 +10,30 @@ const TopNav = () => {
       if (!theme) return null;
 
   return (
-    <div className={`flex-between fixed z-50 w-full gap-5 p-6 ${theme.mode === 'dark' ? 'bg-[#111] text-white' : 'bg-[#eee] text-black shadow-light-300'} sm:px-12`}>
-        <div className='flex items-center px-6 '>
-            <Link href='/' className='font-black text-3xl'>
-            NextText
+    <nav className={`flex items-center justify-between fixed z-50 w-full gap-5 p-6 ${theme.mode === 'dark' ? 'bg-zinc-900 text-white' : 'bg-[#eee] text-black shadow-light-300'} sm:px-12`}>
+        <div className='flex items-center gap-1'>
+            <Link href='/' className='font-black font-spaceGrotesk text-4xl'>
+              <span>Next</span><span className={`${theme.mode==='dark' ? "text-orange-500" : "text-orange-600"}`}>Flow</span>
             </Link>
         </div>
-        <div className='flex items-center gap-10 px-6'>
-        <Link className='text-lg font-mono' href='/'>Home</Link>
-        <Link className='text-lg font-mono' href='/about'>About</Link>
-        <Link className='text-lg font-mono' href='/contact'>Contact</Link>
+        <div className='flex items-center justify-between gap-5'>
+          <h3 className='font-mono'>Theme</h3>
+          <SignedIn className=''>
+            <UserButton afterSignOutUrl='/'
+            appearance={{
+              elements: {
+                avatarBox: "h-16 w-16",
+              },
+              variables: {
+                colorPrimary: "#ff7000",
+              },
+            }}
+            ></UserButton>
+          </SignedIn> 
+
+          <h3>MobileNav</h3>
         </div>
-    </div>
+    </nav>
   )
 }
 

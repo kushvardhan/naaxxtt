6cda036226f46b55fa3ec70c5316f8bfeb315bb7
Kush Vardhan <kushvardhan39797@gmail.com>
2025-05-21 21:14:44 +0530

    feat: refactor LocalSearchBar component to include Input subcomponent and enhance icon positioning

--- src/components/Shared/Search/LocalSearchBar.tsx
+++ src/components/Shared/Search/LocalSearchBar.tsx
@@ -7,6 +7,31 @@ function cn(...classes: (string | boolean | undefined | null)[]) {
   return classes.filter(Boolean).join(" ")
 }
 
+const Input = ({ className, type, variant = "default", isDark = false, ...props }: InputProps) => {
+  const isBare = variant === "bare"
+
+  return (
+    <input
+      type={type}
+      data-slot="input"
+      className={cn(
+        isBare
+          ? cn(
+              "w-full bg-transparent font-mono border-none outline-none focus:outline-none focus:ring-0 focus:border-none shadow-none px-0 py-0 text-lg",
+              isDark ? "text-white placeholder:text-zinc-400" : "text-black placeholder:text-zinc-500 font-mono placeholder:font-semibold"
+            )
+          : cn(
+              "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+              className
+            ),
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+
 interface CustomInputProps {
   route: string;
   iconPosition?: string;
@@ -29,7 +54,9 @@ const LocalSearchBar = ({
         theme.mode === "dark" ? "bg-black" : "bg-zinc-200"
       }  {otherClasses} `}
     >
-      <svg
+      {
+        iconPosition === 'left' && (
+          <svg
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
@@ -46,6 +73,19 @@ const LocalSearchBar = ({
           d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
         />
       </svg>
+        )
+      }
+      <Input
+          placeholder={placeholder}
+          variant="bare"
+          isDark={isDark}
+          spellCheck={false}
+          autoComplete="on"
+          type='text'
+
+          value=""
+          onChange={()=>{}}
+        />
     </div>
   );
 };

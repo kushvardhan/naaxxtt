43d43597e9bba583e136b2327d674eec2f8b4d40
Kush Vardhan <kushvardhan39797@gmail.com>
2025-05-22 23:16:23 +0530

    feat: enhance tag input handling to allow up to 5 tags and improve submission feedback

--- src/components/forms/Question.tsx
+++ src/components/forms/Question.tsx
@@ -11,7 +11,6 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { useFormContext, FormProvider } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Editor } from "@tinymce/tinymce-react";
 
@@ -21,50 +20,52 @@ import { z } from "zod";
 import { ThemeContext } from "../../../context/ThemeContext";
 import { QuestionSchema } from "../../../lib/validations";
 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
+const type: any = "create";
 
 export function Question() {
   const editorRef = useRef(null);
-  const log = () => {
-    if (editorRef.current) {
-      console.log(editorRef.current.getContent());
-    }
-  };
+
+  const [isSubmmitting, setIsSubmitting] = useState(false);
 
   const theme = useContext(ThemeContext);
   const isDark = theme.mode === "dark";
 
-  const handleInputKeyDown = (e:React.KeyboardEvent<HTMLElement>,field:any)=>{
-    if(e.key === "Enter" && field.name==='tags'){
+  const handleInputKeyDown = (
+    e: React.KeyboardEvent<HTMLElement>,
+    field: any
+  ) => {
+    if (e.key === "Enter" && field.name === "tags") {
       e.preventDefault();
 
       const tagInput = e.target as HTMLInputElement;
       const tagValue = tagInput.value.trim();
 
-      if(tagValue !== ""){
-        if(tagValue.length > 15){
-          return form.setError('tags',{
-            type:'required',
-            message:'Tag must be less than 15 characters.',
-          })
+      if (field.value.length >= 5) {
+        form.setError("tags", {
+          type: "manual",
+          message: "You can add up to 5 tags only.",
+        });
+        return;
+      }
+
+      if (tagValue !== "") {
+        if (tagValue.length > 15) {
+          return form.setError("tags", {
+            type: "required",
+            message: "Tag must be less than 15 characters.",
+          });
         }
 
-        if(!field.value.includes(tagValue)){
-          form.setValue('tags',[...field.value,tagValue]);
+        if (!field.value.includes(tagValue)) {
+          form.setValue("tags", [...field.value, tagValue]);
           tagInput.value = "";
-          form.clearErrors('tags');
-        }else{
+          form.clearErrors("tags");
+        } else {
           form.trigger();
         }
-
       }
     }
-     
-  }
+  };
 
   const form = useForm<z.infer<typeof QuestionSchema>>({
     resolver: zodResolver(QuestionSchema),
@@ -77,9 +78,16 @@ export function Question() {
 
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof QuestionSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+    setIsSubmitting(true);
+  
+    try{
+
+    }catch(error){
+
+    }finally{
+      setIsSubmitting(false);
+    }
+
   }
   return (
     <Form {...form}>
@@ -224,55 +232,59 @@ export function Question() {
                 </span>{" "}
               </FormLabel>
               <FormControl className="mt-1">
-                <>
-
-                    <Input
-                  className={`font-mono text-lg no-focus outline-none min-h-[56px] paragraph ${
-                    isDark
-                      ? "text-white border-2 border-zinc-700 bg-zinc-900"
-                      : "text-black border-1 border-zinc-500/50 bg-zinc-900"
-                  }  !important`}
-                  placeholder="Add tags"
-                  onKeyDown={(e)=>handleInputKeyDown(e,field)}
-                />
-                {field.value.length > 0 && (
-  <div className="flex flex-wrap gap-2 mt-2">
-    {field.value.map((tag: string, index: number) => (
-      <div
-        key={tag}
-        className={`flex items-center gap-1 px-2 py-[0.8] rounded-md text-sm font-medium 
+                <div>
+                  <Input
+                    className={`font-mono text-lg no-focus outline-none min-h-[56px] paragraph ${
+                      isDark
+                        ? "text-white border-2 border-zinc-700 bg-zinc-900"
+                        : "text-black border-1 border-zinc-500/50 bg-zinc-900"
+                    }  !important`}
+                    placeholder="Add tags"
+                    onKeyDown={(e) => handleInputKeyDown(e, field)}
+                  />
+                  {field.value.length > 0 && (
+                    <div className="flex flex-wrap gap-2 mt-2">
+                      {field.value.map((tag: string, index: number) => (
+                        <div
+                          key={tag}
+                          className={`flex items-center gap-1 px-2 py-[0.8] rounded-md text-sm font-medium 
         border transition cursor-pointer group 
-        ${isDark 
-          ? "bg-zinc-800 text-white border-zinc-600 hover:bg-zinc-700" 
-          : "bg-orange-100 text-orange-800 border-orange-300 hover:bg-orange-200"
+        ${
+          isDark
+            ? "bg-zinc-800 text-white border-zinc-600 hover:bg-zinc-700"
+            : "bg-orange-100 text-orange-800 border-orange-300 hover:bg-orange-200"
         }`}
-      >
-        <span>{tag}</span>
-        <span
-          onClick={() => {
-            const updatedTags = [...field.value];
-            updatedTags.splice(index, 1);
-            form.setValue("tags", updatedTags);
-            form.trigger();
-          }}
-          className={`ml-1 text-base font-bold transition 
-            ${isDark ? "text-zinc-400 hover:text-red-400" : "text-zinc-700 hover:text-red-600"}
+                        >
+                          <span>{tag}</span>
+                          <span
+                            onClick={() => {
+                              const updatedTags = [...field.value];
+                              updatedTags.splice(index, 1);
+                              form.setValue("tags", updatedTags);
+                              form.trigger();
+                            }}
+                            className={`ml-1 text-base font-bold transition 
+            ${
+              isDark
+                ? "text-zinc-400 hover:text-red-400"
+                : "text-zinc-700 hover:text-red-600"
+            }
           `}
-        >
-          ×
-        </span>
-      </div>
-    ))}
-  </div>
-)}
-                </>
+                          >
+                            ×
+                          </span>
+                        </div>
+                      ))}
+                    </div>
+                  )}
+                </div>
               </FormControl>
               <FormDescription
                 className={`text-sm mt-2 ${
                   isDark ? "text-zinc-400" : "text-zinc-700/90"
                 }`}
               >
-                Add up to 3 tags to describe what your question is about. You
+                Add up to 5 tags to describe what your question is about. You
                 need to press enter to add a tag.
               </FormDescription>
               <FormMessage />
@@ -280,18 +292,25 @@ export function Question() {
           )}
         />
 
-        <Button
-          className={`rounded-md font-bold text-lg
+        <div className="w-full flex items-center justify-center ">
+          <Button
+            disabled={isSubmmitting}
+            className={`w-[90%] py-5 rounded-md font-bold text-lg mb-10 no-focus border-none outline-none focus:0
         bg-gradient-to-r from-orange-400 to-orange-600
         hover:from-orange-600 hover:to-orange-700
         cursor-pointer
         text-white shadow-md transition
         focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 !important
       `}
-          type="submit"
-        >
-          Ask a Question
-        </Button>
+            type="submit"
+          >
+            {isSubmmitting ? (
+              <>{type === "edit" ? "Editing..." : "Posting..."}</>
+            ) : (
+              <>{type === "edit" ? "Edit Question" : "Ask Question"}</>
+            )}
+          </Button>
+        </div>
       </form>
     </Form>
   );

3f53e6684ac947fc74b74a9b50b723d5e10710ed
Kush Vardhan <kushvardhan39797@gmail.com>
2025-06-15 20:59:33 +0530

    fix: Remove redundant return statement in ask-question page component

--- src/app/(root)/ask-question/page.tsx
+++ src/app/(root)/ask-question/page.tsx
@@ -30,19 +30,6 @@ const Page = async () => {
       </div>
     );
   }
-
-  return (
-    <div className="mt-20 max-h-[80vh] overflow-y-auto scrollbar-none">
-      <h1
-        className={`text-3xl font-bold font-mono  text-black !important dark:text-white `}
-      >
-        Ask a Question
-      </h1>
-      <div className="mt-8 px-2">
-        <Question mongoUserId={mongoUser._id} />
-      </div>
-    </div>
-  );
 };
 
 export default Page;

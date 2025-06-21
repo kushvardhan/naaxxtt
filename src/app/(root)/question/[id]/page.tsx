2a7a171f46bd3e360da76c170c3ab793e68d9f7d
Kush Vardhan <kushvardhan39797@gmail.com>
2025-06-21 21:54:22 +0530

    feat: Add answer action file for future implementation of answer-related functionalities

--- src/app/(root)/question/[id]/page.tsx
+++ src/app/(root)/question/[id]/page.tsx
@@ -2,6 +2,21 @@ import Link from "next/link";
 import QuestionClient from "@/components/Shared/QuestionClient";
 import {getQuestionById} from "../../../../../lib/actions/question.action"
 
+import Image from "next/image";
+
+interface QuestionCardProps {
+  question: {
+    _id: string;
+    title: string;
+    tags: { _id: string; name: string }[];
+    user: { name: string; image: string };
+    upvotes: number;
+    answers: number;
+    views: number;
+    createdAt: string;
+  };
+}
+
 
 interface QuestionDetailPageProps {
   params: {
@@ -21,7 +36,42 @@ const QuestionDetailPage = async ({ params }: QuestionDetailPageProps) => {
     return (
       <>
       <div className='w-full h-[calc(100vh-130px)] mt-20 overflow-y-scroll scrollbar-hidden'>
-        <QuestionClient question={question} />
+            <div className="p-4 border rounded-md shadow-sm hover:shadow-md transition">
+              <h2 className="text-lg hover:underline font-semibold">{question?.title}</h2>
+        
+              <div className="mt-2 flex gap-2 flex-wrap">
+                {question?.tags?.map((tag) => (
+                  <span
+                    key={tag?._id}
+                    className="bg-gray-200 px-2 py-0.5 text-xs rounded-md"
+                  >
+                    {tag?.name}
+                  </span>
+                ))}
+              </div>
+        
+              <div className="mt-3 flex justify-between text-sm text-gray-600">
+                <div className="flex items-center gap-2">
+                  <Image
+                    src={
+                      question?.user?.image ||
+                      "https://banner2.cleanpng.com/20180416/gbw/avfp7lvmb.webp"
+                    }
+                    alt={question?.author?.name}
+                    width={24}
+                    height={24}
+                    className="w-6 h-6 rounded-full"
+                  />
+                  <span>{question?.author?.name}</span>
+                </div>
+        
+                <div className="flex gap-4 text-xs">
+                  <span>👍 {question?.upvotes}</span>
+                  <span>💬 {question?.answers}</span>
+                  <span>👁️ {question?.views}</span>
+                </div>
+              </div>
+            </div>
       </div>
       </>
     );

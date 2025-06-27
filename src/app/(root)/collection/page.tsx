6d4d777b52008e63bdea8d61982f53f8d012d1a5
Kush Vardhan <kushvardhan39797@gmail.com>
2025-06-27 20:28:47 +0530

    fix: add missing auth import and log collection results for debugging

--- src/app/(root)/collection/page.tsx
+++ src/app/(root)/collection/page.tsx
@@ -2,6 +2,7 @@
 import { getAllUsers } from '../../../../lib/actions/user.action';
 import type { Metadata } from 'next';
 import CollectionPage from '../../../components/Shared/CollectionPage';
+import { auth } from '@clerk/nextjs';
 
 
 interface SearchParamsProps {
@@ -13,12 +14,13 @@ export const metadata: Metadata = {
 };
 
 const Page = async ({ searchParams }: SearchParamsProps) => {
+  const {userId: clerkId} = auth();
   const result = await getAllUsers({
     searchQuery: searchParams.q,
     filter: searchParams.filter,
     page: searchParams.page ? +searchParams.page : 1,
   });
-
+  console.log("Collection Result: ", result);
 
   return (
     <div className="w-full h-[calc(100vh-130px)] mt-20 overflow-y-scroll scrollbar-hidden">

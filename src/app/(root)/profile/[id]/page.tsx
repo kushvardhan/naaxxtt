0b8ae4822f3df73838a7d6fe0e05906eb50bc37e
Kush Vardhan <kushvardhan39797@gmail.com>
2025-07-01 23:23:22 +0530

    feat: enhance user profile page with Stats component and improve layout

--- src/app/(root)/profile/[id]/page.tsx
+++ src/app/(root)/profile/[id]/page.tsx
@@ -3,11 +3,24 @@ import {  auth } from '@clerk/nextjs/server';
 import {  SignedIn } from '@clerk/nextjs';
 import Image from 'next/image'
 import Link from 'next/link'
+import { Button } from '@/components/ui/button';
+import ProfileLink from '@/components/Shared/ProfileLink';
+import Stats from '@/components/Shared/Stats';
 
 interface URLProps {
   params: { id: string }
   searchParams: { [key: string]: string | undefined }
 }
+
+const getJoinedDate = (date: Date): string => {
+  const month = date.toLocaleString('default', { month: 'long' });
+  const year = date.getFullYear();
+
+  const joinedDate = `${month} ${year}`;
+
+  return joinedDate;
+}
+
 
 export default async function Page({ params }: URLProps) {
   const { userId: clerkId } = await auth()
@@ -25,7 +38,7 @@ export default async function Page({ params }: URLProps) {
 
   return (
     <div className="w-full h-[calc(100vh-130px)] mt-20 flex items-center justify-center bg-white dark:bg-black text-black dark:text-white">
-       <div className="flex flex-col-reverse items-start justify-between sm:flex-row">
+          <div className="flex flex-col-reverse items-start justify-between sm:flex-row">
         <div className="flex flex-col items-start gap-4 lg:flex-row">
           <Image 
             src={userInfo?.user.picture}
@@ -35,22 +48,41 @@ export default async function Page({ params }: URLProps) {
             className="rounded-full object-cover"
           />
 
-       <h2 className="h2-bold text-dark100_light900">{userInfo.user.name}</h2>
+          <div className="mt-3">
+            <h2 className="h2-bold text-dark100_light900">{userInfo.user.name}</h2>
             <p className="paragraph-regular text-dark200_light800">@{userInfo.user.username}</p>
- {/* {userInfo.user.location && (
+
+            <div className="mt-5 flex flex-wrap items-center justify-start gap-5">
+              {userInfo.user.portfolioWebsite && (
+                <ProfileLink 
+                  imgUrl="/assets/icons/link.svg"
+                  href={userInfo.user.portfolioWebsite}
+                  title="Portfolio"
+                />
+              )}
+
+              {userInfo.user.location && (
                 <ProfileLink 
                   imgUrl="/assets/icons/location.svg"
                   title={userInfo.user.location}
                 />
-              )} */}
+              )}
 
-               {userInfo.user.bio && (
+                <ProfileLink 
+                  imgUrl="/assets/icons/calendar.svg"
+                  title={getJoinedDate(userInfo.user.joinedAt)}
+                />
+            </div>
+
+            {userInfo.user.bio && (
               <p className="paragraph-regular text-dark400_light800 mt-8">
                 {userInfo.user.bio}
               </p>
             )}
+          </div>
+        </div>
 
-            <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3">
+        <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3">
           <SignedIn>
             {clerkId === userInfo.user.clerkId && (
               <Link href="/profile/edit">
@@ -61,9 +93,16 @@ export default async function Page({ params }: URLProps) {
             )}
           </SignedIn>
         </div>
+      </div>
+      
+      <Stats
+        reputation={userInfo.reputation}
+        totalQuestions={userInfo.totalQuestions}
+        totalAnswers={userInfo.totalAnswers}
+        badges={userInfo.badgeCounts}
+      />
 
-</div>
-    </div>
+
     </div>
   )
 }

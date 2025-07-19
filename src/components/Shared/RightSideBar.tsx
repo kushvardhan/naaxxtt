d44a7730ebb412fc00852872ad4d3d59e0b9b356
Kush Vardhan <kushvardhan39797@gmail.com>
2025-07-19 20:01:02 +0530

    changes the RightSideBar code , added icons and package, made a component to use RenderTag

--- src/components/Shared/RightSideBar.tsx
+++ src/components/Shared/RightSideBar.tsx
@@ -1,38 +1,53 @@
-"use server";
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react'
+import RenderTag from './RenderTag';
+import { getHotQuestions } from '../../../lib/actions/question.action';
+import { getTopPopularTags } from '../../../lib/actions/tag.action';
 
-import { getHotQuestions } from "../../../lib/actions/question.action";
-import { getTopPopularTags } from "../../../lib/actions/tag.action";
-import RightSideBarClient from "@/components/Shared/RightSideBarClient";
+const RightSidebar = async () => {
+  const hotQuestions = await getHotQuestions();
+  const popularTags = await getTopPopularTags();
 
+  return (
+    <section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
+      <div>
+        <h3 className="h3-bold text-dark200_light900">Top Questions</h3>
+        <div className="mt-7 flex w-full flex-col gap-[30px]">
+          {hotQuestions.map((question) => (
+            <Link
+              href={`/question/${question._id}`}
+              key={question._id}
+              className='flex cursor-pointer items-center justify-between gap-7'
+            >
+              <p className="body-medium text-dark500_light700">{question.title}</p>
+              <Image 
+                src="/assets/icons/chevron-right.svg"
+                alt="chevron right"
+                width={20}
+                height={20}
+                className="invert-colors"
+              />
+            </Link>
+          ))}
+        </div>
+      </div>
+      <div className="mt-16">
+      <h3 className="h3-bold text-dark200_light900">Popular Tags</h3>
+        <div className="mt-7 flex flex-col gap-4">
+            {popularTags.map((tag) => (
+              <RenderTag 
+                key={tag._id}
+                _id={tag._id}
+                name={tag.name}
+                totalQuestions={tag.numberOfQuestions}
+                showCount
+              />
+            ))}
+        </div>
+      </div>
+    </section>
+  )
+}
 
-const RightSideBar = async () => {
-  try {
-    const hotQuestionsRaw = await getHotQuestions();
-    const tagsRaw = await getTopPopularTags();
-
-    const hotQuestions = hotQuestionsRaw.map((q: any) => ({
-      slug: `/question/${q._id}`,
-      question: q.title,
-    }));
-
-    const popularTags = tagsRaw.map((tag: any) => ({
-      tag: tag.name,
-      count: tag.numberOfQuestions,
-    }));
-
-    console.log("hotQuestions: ", hotQuestions);
-    console.log("popularTags: ", popularTags);
-
-    return (
-      <RightSideBarClient
-        hotQuestions={hotQuestions}
-        popularTags={popularTags}
-      />
-    );
-  } catch (error) {
-    console.error("RightSideBar error:", error);
-    return <RightSideBarClient hotQuestions={[]} popularTags={[]} />;
-  }
-};
-
-export default RightSideBar;
+export default RightSidebar

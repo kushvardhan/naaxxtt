ae749cb76a21133af9bb76436a667dda800c764f
Kush Vardhan <kushvardhan39797@gmail.com>
2025-07-20 23:56:15 +0530

    refactor: update styling and structure in RenderTag and RightSideBarClient components

--- src/components/Shared/RenderTag.tsx
+++ src/components/Shared/RenderTag.tsx
@@ -12,7 +12,7 @@ const RenderTag = ({ _id, name, totalQuestions, showCount }: Props) => {
   return (
     <Link
       href={`/tags/${_id}`}
-      className="group flex items-center justify-between gap-3 rounded-lg border border-zinc-500 dark:border-zinc-300 bg-light-800 dark:bg-dark-300 px-3 py-2 transition-all duration-200 hover:bg-light-700 hover:dark:bg-dark-100"
+      className="group flex items-center justify-between gap-3 rounded-xl border border-zinc-400 dark:border-zinc-500 hover:bg-zinc-200/50 dark:hover:bg-zinc-950 bg-light-800 dark:bg-dark-300 px-2 py-1 transition-all duration-200 hover:bg-light-700 hover:dark:bg-dark-100"
     >
       <Badge className="text-sm font-medium bg-transparent text-light-400 dark:text-light-500 px-0 py-0 border-none">
         #{name}

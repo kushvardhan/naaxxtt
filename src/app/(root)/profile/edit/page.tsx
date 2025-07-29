8f6175b786a9e3fd8f0cee77e17ad5f38abc6149
Kush Vardhan <kushvardhan39797@gmail.com>
2025-07-29 21:48:55 +0530

    removed hardcorded value

--- src/app/(root)/profile/edit/page.tsx
+++ src/app/(root)/profile/edit/page.tsx
@@ -2,24 +2,19 @@ import type { Metadata } from "next";
 import Profile from '../../../../components/forms/Profile';
 import { auth } from "@clerk/nextjs/server";
 import { getUserById } from "../../../../../lib/actions/user.action";
-import Loading from './loading';
 
 export const metadata: Metadata = {
   title: "NullFlow | Edit Profile",
 };
 
-interface PageProps {
-  params: { id: string };
-}
-
-const Page = async ({ params }: PageProps) => {
+const Page = async ({ params }: { params: { id: string } }) => {
   const { userId } = await auth();
+  console.log("User id of edit page: ", userId);
+
   if (!userId) return null;
 
-  const mongoUser = await getUserById({ userId: params.id }); // using params.id, not userId
-
-  const isLoading = false; // temporarily hardcoded for demo
-  if (isLoading) return <Loading />;
+  const mongoUser = await getUserById({ userId });
+  console.log("mongoUser: ", mongoUser);
 
   return (
     <div className='w-full h-[calc(100vh-120px)] mt-20 overflow-y-scroll scrollbar-hidden'>
@@ -32,7 +27,7 @@ const Page = async ({ params }: PageProps) => {
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Page;
